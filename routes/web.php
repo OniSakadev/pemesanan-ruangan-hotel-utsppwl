aae192a308187c318680c0ce2fdf67bde223f7a9
Fikry2121 <tselsquad@gmail.com>
2025-03-25 16:39:21 +0700

    Update- MultiAuth Login

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -14,7 +17,20 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.delete');
 });
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
+
+
+Route::middleware(['auth', 'userMiddleware'])->group(function () {
+
+    Route::get('dashboard', [UserController::class, 'index'])->name('dashboard');
+});
+
+
+Route::middleware(['auth', 'adminMiddleware'])->group(function () {
+
+    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+});
